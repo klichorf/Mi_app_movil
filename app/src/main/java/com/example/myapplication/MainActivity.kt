03c22afef90eb695ecae183b9a2cc780e353f075
Jorge Leonardo <klichorf123@hotmail.com>
2022-10-26 20:30:56 -0500

    ya tinene landscape2

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun registro(btnnewuser: View){
+        val objeto1 = Intent(this,formulActivity::class.java)
+        startActivity(objeto1)
     }
 }
