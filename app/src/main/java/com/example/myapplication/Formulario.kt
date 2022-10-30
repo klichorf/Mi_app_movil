e95a87b1cced75efaa1849548d0fc2fdf458b848
Jorge Leonardo <klichorf123@hotmail.com>
2022-10-30 10:22:21 -0500

    301022:10:22am

--- app/src/main/java/com/example/myapplication/Formulario.kt
+++ app/src/main/java/com/example/myapplication/Formulario.kt
@@ -2,10 +2,74 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_formulario.*
+import kotlinx.android.synthetic.main.activity_formulario.buttonREGIST
+import kotlinx.android.synthetic.main.activity_main.*
 
 class Formulario : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario)
+        btnBack.setOnClickListener { checkValue() }
+
+
+//        TERMS.setOnClickListener { checkagree() }
+
+
+
     }
-}
+
+    fun checkValue(){
+        if (editTxtNAMES.text.isNotEmpty() && editTxtLASTNAMES.text.isNotEmpty() && editTxtMAIL.text.isNotEmpty() && editTxtNEWPASSWORD.text.isNotEmpty()
+            && editTxtCONFIRMPASSW.text.isNotEmpty()  && editTxtPHONE.text.isNotEmpty())
+
+        // vamos a una nueva pantalla
+        else{
+            showErrorName()
+        }
+    }
+
+
+    fun showErrorName(){
+        Toast.makeText(this," field empty",Toast.LENGTH_SHORT).show()
+    }
+
+
+
+
+
+/*    fun checkagree(){
+        if (TERMS.text.isNotEmpty() )
+
+        // vamos a una nueva pantalla
+        else{
+            showErrorcheckagree()
+        }
+    }
+
+    fun showErrorcheckagree(){
+        Toast.makeText(this," Accept the terms and conditions",Toast.LENGTH_SHORT).show()}
+*/    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,40 +3,64 @@ package com.example.myapplication
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private var editTxtUSR:EditText?= null // declaración de variables para recibir usuario
-    private var editTextPassword:EditText?= null // declaración de variables para recibir contraseña
+    private var editTxtUSR: EditText? = null // declaración de variables para recibir usuario
+    private var editTextPassword: EditText? = null // declaración de variables para recibir contraseña
+
+    private lateinit var binding: ActivityMainBinding
+    //la utilizacion del Binding para referencias de vistas o actividades//
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
 
-        editTxtUSR=findViewById(R.id.editTxtUSR) // concatenación parte grafica con lógica
-        editTextPassword=findViewById(R.id.editTextPassword)
+
+
+        binding.buttonREGIST.setOnClickListener { toast() }
+        binding.editTxtUSR.setOnClickListener { usuario_toast() }
+
+
+
+
+
+        editTxtUSR = findViewById(R.id.editTxtUSR) // concatenación parte grafica con lógica
+        editTextPassword = findViewById(R.id.editTextPassword)
+
+
     }
 
 
-    fun regist(btnnewuser: View){
-        val objeto = Intent(this,Formulario::class.java)
+
+    fun regist(btnnewuser: View) {
+        val objeto = Intent(this, Formulario::class.java)
         startActivity(objeto)
     }
 
 
-    fun pantalla(btnmenu: View){
-        val objeto1 = Intent(this,menu::class.java)
-        startActivity(objeto1)
+
+
+
+    private fun toast() {
+
+        Toast.makeText(this, "hello word", Toast.LENGTH_SHORT).show()
     }
 
 
+    private fun usuario_toast() {
 
+        Toast.makeText(this, "ingrese usuario", Toast.LENGTH_SHORT).show()
+    }
 
 
 }
